23a2f3078cb83433659c1a44cbe5f9e8681c0cc4
marcelinaciupak <marcelina.ciupak@gmail.com>
2022-07-25 12:39:30 +0200

    Architecture and add Swagger

--- ContactList/Program.cs
+++ ContactList/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+// Register the Swagger generator
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseSwaggerUI();
+
+app.UseSwagger(x => x.SerializeAsV2 = true);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
